a848c22a3d8dd7b3dc8c64a68a82b9b4cc4ead7d
eric900 <cavanwei@cavanweideiMac.local>
2015-12-02 19:35:35 +0800

    test

---
+++ sub1/sub2/sw2_8_1.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(event) {
+  console.log('install haha4!');
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('activate haha4!');
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+      }
+    )
+  );
+});
