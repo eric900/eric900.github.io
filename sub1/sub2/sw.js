10079cba9f16211f5b3ac72487e6e84d08841450
eric900 <cavanwei@cavanweideiMac.local>
2015-12-17 09:56:16 +0800

    test

--- sub1/sub2/sw.js
+++ sub1/sub2/sw.js
@@ -7,17 +7,17 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
+  // console.log(event.request.url);
+  // event.respondWith(
+  //   caches.match(event.request)
+  //     .then(function(response) {
+  //       // Cache hit - return response
+  //       if (response) {
+  //         return response;
+  //       }
 
-        return fetch(event.request);
-      }
-    )
-  );
+  //       return fetch(event.request);
+  //     }
+  //   )
+  // );
 });
