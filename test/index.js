fef0f9b14d9d33ac3755725a5d81299446a8e9a6
eric900 <cavanwei@cavanweideiMac.local>
2016-05-27 10:31:10 +0800

    test

---
+++ test/index.js
@@ -0,0 +1,29 @@
+function buildImage(){
+        var imgs = document.querySelectorAll(['figure', '.banner a'])
+   for(var i = 0; i< imgs.length; i++){
+      var uri =  imgs[i].getAttribute('uri');
+      console.log(uri);
+      if(uri){
+         imgs[i].style.background = 'url('+uri.replace(/http:\/\//, 'https:\/\/')+'?cors=1)';
+         imgs[i].style.backgroundPosition = 'center'  
+         imgs[i].style.backgroundSize = 'cover';
+      }
+   }
+   document.body.style.display = "block";
+}
+
+ //业务层开始
+
+function init() {
+        buildImage();
+        if ('serviceWorker' in navigator) {
+               navigator.serviceWorker.register('./sw.js').then(function(registration) {
+	             // Registration was successful
+	             console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+	       }).catch(function(err) {
+	            // registration failed :(
+		    console.log('ServiceWorker registration failed: ', err);
+	       });
+	}
+
+}
